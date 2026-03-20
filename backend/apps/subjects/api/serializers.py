2e8e0704ec8d729e8975733f65c908e8c2e5ac8e
Dinesh Kumar <dk2020mailaddress@gmail.com>
2026-03-20 21:40:23 +0530

    Add subtopic serializer

--- backend/apps/subjects/api/serializers.py
+++ backend/apps/subjects/api/serializers.py
@@ -4,14 +4,23 @@
 
 from rest_framework import serializers
 
-from ..models import Subject, Topic
+from ..models import Subject, SubTopic, Topic
+
+
+class SubTopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTopic
+        fields = ['id', 'name', 'description', 'created_at']
+        read_only_fields = ['id', 'created_at']
 
 
 class TopicSerializer(serializers.ModelSerializer):
+    subtopics = SubTopicSerializer(many=True, read_only=True)
+
     class Meta:
         model = Topic
-        fields = ["id", "name", "description", "created_at"]
-        read_only_fields = ["id", "created_at"]
+        fields = ['id', 'name', 'description', 'subtopics', 'created_at']
+        read_only_fields = ['id', 'created_at']
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -19,5 +28,6 @@ class SubjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Subject
-        fields = ["id", "name", "description", "topics", "created_at"]
-        read_only_fields = ["id", "created_at"]
+        fields = ['id', 'name', 'description', 'topics', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
