b1e8ed235b1d849a737a08161417a2c0fb5ffa05
Dinesh Kumar <dk2020mailaddress@gmail.com>
2026-03-28 00:51:23 +0530

    Create exams app

--- backend/examinator/settings/base.py
+++ backend/examinator/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     # Local
     "apps.subjects",
     "apps.questions",
+    "exams"
 ]
 
 MIDDLEWARE = [
---
+++ backend/exams/apps.py
@@ -0,0 +1,10 @@
+# Copyright 2026 BigSecret1
+#
+# Licensed under the Apache License, Version 2.0
+
+from django.apps import AppConfig
+
+
+class ExamsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'exams'
