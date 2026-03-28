bec2d1ede737eb200ee283bdd193041cea59f17e
BigSecret1 <dk2020mailaddress@gmail.com>
2026-03-28 22:35:48 +0530

    Implement API view for daily exam questions

--- backend/exams/api/urls.py
+++ backend/exams/api/urls.py
@@ -1,8 +1,15 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ExamViewSet
+from .views import ExamSubjectQuestionAPIView, ExamViewSet
 
 router = DefaultRouter()
 router.register('', ExamViewSet, basename='exam')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(
+        '<int:exam_id>/daily-questions/',
+        ExamSubjectQuestionAPIView.as_view(),
+        name='exam-daily-questions',
+    ),
+] + router.urls
